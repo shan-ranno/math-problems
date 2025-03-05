b4ef437530f998da0b7fb7dc2cd46fb72aee7c61
shan-ranno <shan-ranno@gmx.com>
2025-03-05 04:17:16 +0000

    Add 1.c

---
+++ src/1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    int a = 5;
+    int b = 10;
+    int c = a + b;
+    printf("The sum of %d and %d is %d.\n", a, b, c);
+    return 0;
+}
